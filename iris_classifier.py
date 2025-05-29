6bf4071181991b1a8415ac9b48de9f0a3f295ec3
Muhammad Awais <owaiskhan6605@gmail.com>
2025-05-29 11:59:43 +0500

    data visualization: pairplot & heatmap

--- iris_classifier.py
+++ iris_classifier.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 from sklearn.datasets import load_iris
 
 iris = load_iris()
@@ -14,4 +16,15 @@ df['target'] = iris.target
 print(df.head())
 print(df.info())
 print(df.describe())
+
+sns.pairplot(df, hue='target', palette='husl')
+plt.show()
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+plt.title('Feature Correlation Heatmap')
+plt.show()
+
+
+
 
